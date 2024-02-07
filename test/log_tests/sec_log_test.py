6f04b143a27abeee51d8dc174c6f17fcdb45e596
MikeDQG <mike.govedic@gmail.com>
2024-02-07 12:11:47 +0100

    maintenance commit

---
+++ test/log_tests/sec_log_test.py
@@ -0,0 +1,5 @@
+import logging
+
+def do_smth():
+    logging.critical('well well well')
+    print("any1 here?")
--- test/sec_log_test.py
+++
@@ -1,4 +0,0 @@
-import logging
-
-def do_smth():
-    logging.critical('well well well')
