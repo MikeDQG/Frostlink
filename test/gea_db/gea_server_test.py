e5cc64386b428ff64995c421c4de80931ce8b78c
MikeDQG <mike.govedic@gmail.com>
2024-02-09 13:24:35 +0100

    cmt 05

--- test/gea_db/gea_server_test.py
+++ test/gea_db/gea_server_test.py
@@ -2,14 +2,15 @@ import pyodbc
 import logging
 
 logging.basicConfig(filename='dbsrvrtst.log', filemode='w', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(module)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+logging.info("Start")
 
 # Connection string
-server = 'TOGEA15\\SQLEXPRESS' # Replace 'your_server_name' with your server name
+server = r'TOGEA15\SQLEXPRESS' # Replace 'your_server_name' with your server name
 database = 'Energetika' # Replace 'your_database_name' with your database name
 username = 'sa' # Replace 'your_username' with your SQL Server username
 password = 'togea' # Replace 'your_password' with your SQL Server password
 
-logging.debug(server, database, username, password)
+#logging.debug(server, database, username, password)
 
 conn_str = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
 logging.debug(conn_str)
@@ -30,6 +31,7 @@ try:
 
 except Exception as e:
     print(f"Error: {str(e)}")
+    logging.error(e)
 
 finally:
     # Close the connection
