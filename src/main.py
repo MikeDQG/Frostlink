70ba2ef8b6e05369c851fe715389dab1fbb2ef8d
MikeDQG <mike.govedic@gmail.com>
2024-02-11 22:42:33 +0100

    la esperanza

--- src/main.py
+++ src/main.py
@@ -1,18 +1,43 @@
 import reporter
 import logging
+import time
 
 
 class Main():
     def __init__(self):
         #logging.basicConfig(filename='./src/logs/example.log', filemode='w', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
-        #logging.basicConfig(filename='example.log', filemode='w', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
-        logging.debug("Main initialized")
+        logging.basicConfig(filename='final_test_1.log', filemode='w', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(module)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
         self.reporter = reporter.Reporter()
-        self.run()
+        logging.debug("Main initialized")
+        #print(input("press any key to start ")) # stoopid test
+        try:
+            self.run()
+        except KeyboardInterrupt as KI:
+            logging.exception(KI)
+            self.reporter.retriever.end_session()
 
-    def run(self):
-        self.reporter.main()
+    def run(self):      # timer logic
+        relative_time = 300
+        t = time.time()
+        STEPPER = 3
+        TOTAL_TIME = time.time() + relative_time
+        last_time = time.time()
+        t = time.time()
+        #print(t, last_time, relative_time, STEPPER, TOTAL_TIME)
 
-'''
+        while t < TOTAL_TIME:
+            if (t - last_time) > STEPPER:
+                self._run()
+                last_time = t
+            t = time.time()
+
+
+    def _run(self):     # run main program
+        try:
+            #print("joke")
+            self.reporter.main()
+        except Exception as e:
+            logging.exception(e)
+
+
 m = Main()
-m.run()'''
--- test/main_test.py
+++ test/main_test.py
@@ -1,22 +1,40 @@
 import main
 import logging
+import time
+ 
+seconds = time.time()
+t = time.time()
+
 
 def test0():
-    logging.basicConfig(filename='example.log', filemode='w', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(module)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
-    print(input())
+    print(input("press any key to start ")) # stoopid test
+    logging.basicConfig(filename='final_test_1.log', filemode='w', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(module)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+    
     logging.info("App started")
-    m1 = main.Main()
+    #m1 = main.Main()
     
-    i = ''
-    while i != 'e':
-        try:
-            m1.run()
-        except Exception as exec:
-            print(exec)
-
-        i = input("next command (refresh - r, exit - e): ")
+    try:
+        #m1.run()
+        print("Smth")
+    except Exception as exec:
+        print(exec)
+            
+    #m1.reporter.retriever.end_session()
     
-    m1.reporter.retriever.end_session()
+    print(input("press any key to close")) # stoopid test #2
 
+#test0()
 
-test0()
+relative_time = 300
+t = time.time()
+STEPPER = 3
+TOTAL_TIME = time.time() + relative_time
+last_time = time.time()
+t = time.time()
+print(t, last_time, relative_time, STEPPER, TOTAL_TIME)
+
+while t < TOTAL_TIME:
+    if (t - last_time) > STEPPER:
+        test0()
+        last_time = t
+    t = time.time()
