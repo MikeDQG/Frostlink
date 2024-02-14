086b259b0bd81ddc50183cdfa4b39a8151e4e916
MikeDQG <mike.govedic@gmail.com>
2024-02-14 18:23:58 +0100

    cmt 15

--- test/gea_db/gea_server_test.py
+++ test/gea_db/gea_server_test.py
@@ -1,7 +1,7 @@
 import pyodbc
 import logging
 
-logging.basicConfig(filename='dbsrvrtst.log', filemode='a', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(module)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+logging.basicConfig(filename='dbsrvrtst_11.log', filemode='a', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(module)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
 logging.info("Start")
 
 # Connection string
@@ -17,15 +17,15 @@ logging.debug(conn_str)
 
 print(input("press any key to start ")) # stoopid test
 
-try:
-    # Connect to the database
-    conn = pyodbc.connect(conn_str)
-    cursor = conn.cursor()
+def select_version():
+    query_string = "SELECT @@version;"
+    cursor.execute(query_string)
+    logging.debug(query_string)
+    verr = cursor.fetchone()
+    print(verr)
+    logging.info(verr)
 
-    # Execute SQL query
-    '''cursor.execute("SELECT @@version;")'''
-    #cursor.execute("UPDATE RafHA_Komun SET Utrip = 1")
-    
+def update():
     # Update query
     update_query = """
     UPDATE RafHA_Komun
@@ -50,6 +50,22 @@ try:
     logging.debug(row)
     logging.debug(row[0])
     print(row[0])  # Print the result'''
+
+try:
+    # Connect to the database
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+
+    select_version()
+    '''
+    s = input("press any 1 to select ")
+    if s == "1":
+        logging.debug(1)
+        select_version()
+    else:
+        logging.debug(0)
+        update()
+    '''
 
 except Exception as e:
     print(f"Error: {str(e)}")
