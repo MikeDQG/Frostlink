03d1f729d984cda28cd45110b48f45edbd81437e
MikeDQG <mike.govedic@gmail.com>
2024-02-14 19:36:38 +0100

    same

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import time
 class Main():
     def __init__(self):
         #logging.basicConfig(filename='./src/logs/example.log', filemode='w', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
-        logging.basicConfig(filename='final_main_3.log', filemode='a', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(module)s %(levelname)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+        logging.basicConfig(filename='final_main_1.log', filemode='a', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(module)s %(levelname)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
         self.reporter = reporter.Reporter()
         logging.debug("Main initialized")
         print(input("press any key to start ")) # stoopid test
